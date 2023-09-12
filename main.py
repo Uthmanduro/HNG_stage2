33b981a434b26d46c4fae7bb35904cac0df7475d
uthmanduro <udurosinlohun@gmail.com>
2023-09-12 19:06:05 +0100

    changed api route

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.post("/user/", response_model=schemas.User, status_code=status.HTTP_201_CREATED)
+@app.post("/api/", response_model=schemas.User, status_code=status.HTTP_201_CREATED)
 def create_user(user: schemas.User, db: Session = Depends(get_db)):
     db_user = User(name=user.name)
     db.add(db_user)
@@ -17,19 +17,19 @@ def create_user(user: schemas.User, db: Session = Depends(get_db)):
     db.refresh(db_user)
     return db_user
 
-@app.get("/user/", status_code=200)
+@app.get("/api/", status_code=200)
 def get_users(db: Session = Depends(get_db)):
     users = db.query(User).all()
     return users
 
-@app.get("/user/{user_name}", status_code=200, response_model=schemas.User)
+@app.get("/api/{user_name}", status_code=200, response_model=schemas.User)
 def get_user(user_name: str, db: Session = Depends(get_db)):
     db_user = db.query(User).filter(User.name == user_name).first()
     if db_user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User  is not found")
     return db_user
 
-@app.put("/user/{user_name}", response_model=schemas.User, status_code=status.HTTP_200_OK)
+@app.put("/api/{user_name}", response_model=schemas.User, status_code=status.HTTP_200_OK)
 async def update_user(user_name: str, new_user: schemas.User, db: Session = Depends(get_db) ):
     db_user = db.query(User).filter(User.name == user_name).first()
     if db_user is None:
@@ -38,7 +38,7 @@ async def update_user(user_name: str, new_user: schemas.User, db: Session = Depe
     db.commit()
     return db_user
 
-@app.delete("/user/{user_name}", status_code=status.HTTP_204_NO_CONTENT)
+@app.delete("/api/{user_name}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_user(user_name: str, db: Session = Depends(get_db)):
     db_user = db.query(User).filter(User.name == user_name).first()
     if db_user is None:
