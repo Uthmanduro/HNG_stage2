7731ac5d2953f0fd9446fbff9addc2b2e73a99d3
uthmanduro <udurosinlohun@gmail.com>
2023-09-12 21:51:42 +0100

    used only name

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String
 
 from db import Base
 
@@ -6,5 +6,4 @@ from db import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
-    name = Column(String, nullable=False, unique=True)
+    name = Column(String, nullable=False, unique=True, primary_key=True)
--- schemas.py
+++ schemas.py
@@ -1,12 +1,10 @@
 from pydantic import BaseModel, Field
-from typing import Optional
 
 class UserCreate(BaseModel):
     name: str = Field(..., example="John Doe")
 
 
 class User(BaseModel):
-    id: Optional[int] = None
     name: str = Field(..., example="John Doe")
 
     class Config:
